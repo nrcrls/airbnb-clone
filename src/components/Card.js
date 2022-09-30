803662b05fc4a5c81b3b93937c766ffdf810d8c5
Nikki Carlos <nikkirosecarlos@gmail.com>
2022-09-30 17:07:14 +0900

    created card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,19 @@
 import suit from '../assets/suit.jpg';
+import star from '../assets/star.png';
 
 function Card() {
   return (
     <div className='card'>
-      <img src={suit} className="card-photo" alt="suit" />
+      <img className="card-photo" src={suit} alt="suit" />
+      <div className='card-stats'>
+        <img className="card-star" src={star} alt="star" />
+        <span>5.0</span>
+        <span>(6) • </span>
+        <span>Meguro</span>
+      </div>
+      <p><span className="bold">The perfect suit for a pitch!</span></p>
+      <p>This black ensemble will have the crowd mesmerized.</p>
+      <p><span className="bold">¥2,000</span> per day</p>
     </div>
   );
 }
