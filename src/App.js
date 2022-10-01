c3cd706c21f384ef9cb5eeae5dcbc1e2fd015dfe
Nikki Carlos <nikkirosecarlos@gmail.com>
2022-10-01 14:02:41 +0900

    started mapping components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
-
+import suit from "./assets/suit.jpg";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
-      <Card />
+      <Card
+        img={suit}
+        rating={5.0}
+        reviewCount={6}
+        location="Meguro"
+        title="The perfect suit for a pitch!"
+        description="This black ensemble will have the crowd mesmerized."
+        price={2000}
+      />
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,18 @@
-import suit from '../assets/suit.jpg';
 import star from '../assets/star.png';
 
-function Card() {
+function Card({ img, rating, reviewCount, location, title, description, price }) {
   return (
     <div className='card'>
-      <img className="card-photo" src={suit} alt="suit" />
+      <img className="card-photo" src={img} alt="suit" />
       <div className='card-stats'>
         <img className="card-star" src={star} alt="star" />
-        <span>5.0</span>
-        <span>(6) • </span>
-        <span>Meguro</span>
+        <span>{rating}</span>
+        <span>({reviewCount}) • </span>
+        <span>{location}</span>
       </div>
-      <p><span className="bold">The perfect suit for a pitch!</span></p>
-      <p>This black ensemble will have the crowd mesmerized.</p>
-      <p><span className="bold">¥2,000</span> per day</p>
+      <p><span className="bold">{title}</span></p>
+      <p>{description}</p>
+      <p><span className="bold">¥{price}</span> per day</p>
     </div>
   );
 }
